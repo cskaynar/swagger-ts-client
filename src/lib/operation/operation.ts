67666d41765021d06435b856e44dc8bc66c8be00
Cem Sinan Kaynar <ckaynar@MagiClick.com>
2019-11-27 13:29:01 +0300

    inHeader propery added to param

--- src/lib/operation/operation.ts
+++ src/lib/operation/operation.ts
@@ -48,6 +48,7 @@ export class Operation implements IOperation {
             inBody: param.in === "body",
             inPath: param.in === "path",
             inQuery: param.in === "query",
+            inHeader: param.in === "header",
         } as IOperationParam;
     }
 
